4520ef403cea6032dadb0142d8d902d656946e89
ChrisTHC <48893623+ChrisTHChan@users.noreply.github.com>
2023-10-31 16:56:06 -0400

    creating handle stripe integration init

---
+++ frontend/src/app/pricing/_components/Card.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+type props = {
+    header: string,
+    price: number,
+    includeButton: boolean,
+    listItems: string[],
+    headerClasses?: string
+}
+
+import PrimaryButton from "../../../components/primaryButton"
+import Cookies from 'js-cookie'
+
+const handleStripeSubmit = async () => {
+
+    const res = await fetch(`http://localhost:9000/stripe/handleSubscription/${Cookies.get('QUIZZIP-AUTH')}`, {
+        method: 'POST'
+    })
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    const response = await res.json()
+
+    console.log(response.requestStatus);
+
+    return response
+}
+
+const Card = ({header, price, includeButton, listItems, headerClasses}: props) => {
+
+    return (
+        <>
+            <div className="rounded-lg w-full sm:w-1/2 border-2 border-slate-600 p-6">
+                <h4 className={`${headerClasses} text-2xl font-semibold mb-4`}>{header}</h4>
+                <h5 className="mb-4 text-lg font-semibold">${price}/month</h5>
+                <ul className="text-sm">
+                    {listItems.map((item, i) => {
+                        return <li className="mb-1 list-disc ml-4" key={i}>{item}</li>
+                    })}
+                </ul>
+                {includeButton ? <PrimaryButton onClick={handleStripeSubmit} extra_classes="mt-4">Choose this plan</PrimaryButton>: null}
+            </div>
+        </>
+    )
+}
+
+export default Card
--- frontend/src/app/pricing/page.tsx
+++ frontend/src/app/pricing/page.tsx
@@ -1,29 +1,4 @@
-type props = {
-    header: string,
-    price: number,
-    includeButton: boolean,
-    listItems: string[],
-    headerClasses?: string
-}
-
-import PrimaryButton from "../../components/primaryButton"
-
-const Card = ({header, price, includeButton, listItems, headerClasses}: props) => {
-    return (
-        <>
-            <div className="rounded-lg w-full sm:w-1/2 border-2 border-slate-600 p-6">
-                <h4 className={`${headerClasses} text-2xl font-semibold mb-4`}>{header}</h4>
-                <h5 className="mb-4 text-lg font-semibold">${price}/month</h5>
-                <ul className="text-sm">
-                    {listItems.map((item, i) => {
-                        return <li className="mb-1 list-disc ml-4" key={i}>{item}</li>
-                    })}
-                </ul>
-                {includeButton ? <PrimaryButton extra_classes="mt-4">Choose this plan</PrimaryButton>: null}
-            </div>
-        </>
-    )
-}
+import Card from "./_components/Card"
 
 export default function Pricing() {
 
