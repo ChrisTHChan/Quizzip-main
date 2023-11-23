5d2adeceb8724efa2687d12b4b24c0ee3046b57f
ChrisTHC <48893623+ChrisTHChan@users.noreply.github.com>
2023-11-23 05:56:04 -0500

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -22,31 +22,31 @@ app.use(fileUpload());
 app.use(compression());
 app.use(cookieParser());
 
-// console.log(app.get('env'))
+console.log(app.settings.env)
 
-// let io:Server
-// if (app.get('env') === 'development') {
+let io:Server
+if (app.settings.env === 'development') {
 
-//     console.log('in development mode');
+    console.log('in development mode');
 
-//     app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+    app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
-//     io =  new Server(server, {
-//         cors: {
-//             origin: "http://localhost:3000",
-//             methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//         },
-//         path: '/socket.io'
-//     })
-// } else {
-//     io = new Server(server, {
-//         path: '/socket.io/',
-//     });
-// }
+    io =  new Server(server, {
+        cors: {
+            origin: "http://localhost:3000",
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        },
+        path: '/socket.io'
+    })
+} else {
+    io = new Server(server, {
+        path: '/socket.io/',
+    });
+}
 
-const io = new Server(server, {
-    path: '/socket.io/',
-});
+// const io = new Server(server, {
+//     path: '/socket.io/',
+// });
 
 app.use(express.json({limit: '1mb'}));
 server.listen(9000, () => {
