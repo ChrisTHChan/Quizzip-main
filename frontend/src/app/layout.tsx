8cb557cb730f1ed64a426f7fbb838cfd9fc7719c
ChrisTHC <48893623+ChrisTHChan@users.noreply.github.com>
2023-12-02 22:14:50 -0500

    making it nice

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,9 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Image from 'next/image';
 import logo from '../../public/images/logo.png';
-import PrimaryButton from '../components/primaryButton';
 import Link from 'next/link';
-import SecondaryButton from '../components/secondaryButton';
 import HeaderProfile from '@/components/headerProfile';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,9 +21,9 @@ export default function RootLayout({
   
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-900 text-slate-200`}>
-        <header className="flex justify-center items-center">
-          <div className="container py-6 mx-4 border-b flex flex-col sm:flex-row justify-between items-center mb-4 sm:mb-16">
+      <body className={`${inter.className} bg-slate-900 text-neutral-300`}>
+        <header className="flex justify-center items-center border-b sm:mb-16">
+          <div className="container py-6 mx-4 flex flex-col sm:flex-row justify-between items-center">
             <Link href="/" className="flex gap-3 items-center mb-4 sm:mb-0">
               <div className="h-12 w-10">
                 <Image alt="a test" src={logo} className="object-contain h-full"/>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,8 @@
 import PrimaryButton from '@/components/primaryButton';
+import Link from 'next/link'
+import Image from 'next/image';
+import ss1 from '../../public/images/ss1.png';
+import ss2 from '../../public/images/ss2.png'
 
 export default function Home() {
 
@@ -6,12 +10,52 @@ export default function Home() {
     <>
       <div className="py-10 flex justify-center items-center">
         <div className="container">
+
           <div className="mb-16 container w-11/12 md:w-3/5 xl:w-2/5 mx-auto">
             <h2 className="text-xl font-semibold text-center mb-4">Quickly create assessments from any educational content.</h2>
             <h3 className="text-6xl font-extrabold text-center mb-4">Get back <span className="text-blue-500">hours of your day.</span></h3>
             <h4 className="text-center mb-4 text-slate-400">Quizzip is a tool that accurately and intelligently creates learning assessments based on the educational content you feed it.</h4>
-            <PrimaryButton extra_classes="block mx-auto">Get Started Now</PrimaryButton>
+            <div className="w-full flex justify-center">
+              <Link className="inline-block mx-auto" href="/create"><PrimaryButton>Get Started</PrimaryButton></Link>
+            </div>
           </div>
+
+          <div className="flex gap-4 mb-48">
+            <div className="h-auto w-10/12 mx-auto rounded border-2 border-slate-700">
+              <Image alt="a test" src={ss1} className="object-contain h-full"/>
+            </div>
+          </div>
+
+          <div className="mb-48">
+            <p className="text-center mb-16 text-slate-400 text-sm font-semibold uppercase">Made for educators, businesses, and students everywhere.</p>
+            <div className="w-full md:flex md:w-10/12 mx-auto mb-2 px-6 md:px-0 gap-16 justify-between">
+              <div className="md:w-1/3">
+                <p className="font-semibold mb-4">Educators</p>
+                <p className="text-slate-300 text-sm leading-loose">Elevate your teaching with AI-powered quiz, assessment, and exam generation. Save time on content creation, grading, and enhance student engagement.</p>
+                <Link href="/create" className="block my-4 md:hidden"><button className="text-sm font-semibold text-blue-400 hover:underline underline-offset-8">Get Started &#8594;</button></Link>
+              </div>
+              <div className="md:w-1/3">
+                <p className="font-semibold mb-4">Businesses</p>
+                <p className="text-slate-300 text-sm leading-loose">Create assessments in no time to boost knowledge and evaluate skills. Whether it's for delivering trainings, developing courses, or certification - we've got you covered.</p>
+                <Link href="/create" className="block my-4 md:hidden"><button className="text-sm font-semibold text-blue-400 hover:underline underline-offset-8">Get Started &#8594;</button></Link>
+              </div>
+              <div className="md:w-1/3">
+                <p className="font-semibold mb-4">Students</p>
+                <p className="text-slate-300 text-sm leading-loose">Learn smarter. Enjoy AI-generated quizzes, flashcards, and notes tailored to your unique learning needs. Save time, boost retention, and ace your exams with ease.</p>
+                <Link href="/create" className="block my-4 md:hidden"><button className="text-sm font-semibold text-blue-400 hover:underline underline-offset-8">Get Started &#8594;</button></Link>
+              </div>
+            </div>
+            <div className="hidden md:flex w-10/12 mx-auto gap-16 justify-between">
+              <div className="w-1/3"><Link href="/create"><button className="text-sm font-semibold text-blue-400 hover:underline underline-offset-8">Get Started &#8594;</button></Link></div>
+              <div className="w-1/3"><Link href="/create" className="w-1/3"><button className="text-sm font-semibold text-blue-400 hover:underline underline-offset-8">Get Started &#8594;</button></Link></div>
+              <div className="w-1/3"><Link href="/create" className="w-1/3"><button className="text-sm font-semibold text-blue-400 hover:underline underline-offset-8">Get Started &#8594;</button></Link></div>
+            </div>
+          </div>
+
+          <div className="px-6">
+            fjdklafmdkalfdla
+          </div>
+
         </div>
       </div>
     </>
