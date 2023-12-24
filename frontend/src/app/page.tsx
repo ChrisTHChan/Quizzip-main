8b6035ef59bfbc8100559bdd0f3afc3e3723a454
ChrisTHC <48893623+ChrisTHChan@users.noreply.github.com>
2023-12-23 20:16:21 -0500

    finishing faq content

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -122,11 +122,77 @@ export default function Home() {
             <div className="w-full md:w-10/12 mx-auto flex flex-col lg:flex-row flex-wrap justify-between">
               <div className="lg:w-1/2">
                 <Accordion 
-                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">hello world</div>}
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">How does QuizzipIO work?</div>}
                   initialContentClasses='w-full text-left'
                 >
-                  <div className="text-sm text-slate-300 px-4 pb-4">
-                    <p>fjdklaj fdklajf dkla fjdkla jfdkla jfkdl;a jfkld;a jfkld;a jfdkla; jfldk afjkdl fkld;a jfklda fjdkl;aj fdkla;sj fklad;j fdkl;aj fdkl;a jfdkl; jfkdl;a fjdkla; jfdkl; jfdkl; ajfdkl;a jkldf;as</p>
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">QuizzipIO's AI-powered question and answer generator works by first analyzing and extracting the key facts from any text. Then we use the same AI that powers ChatGPT to generate different types of questions and answers.</p>
+                    <p className="mb-4">Whether you have a piece of educational content, a webpage, a YouTube video, or even a PDF or Word file, QuizzipIO can transform it into a quiz or assessment.</p>
+                    <p className="mb-4">To get started, simply input/upload your text or the URL to the content you want to turn into a quiz. QuizzipIO will then analyze the text and generate relevant questions for you.</p>
+                  </div>
+                </Accordion>
+              </div>
+              <div className="lg:w-1/2">
+                <Accordion 
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">What can I use QuizzipIO for?</div>}
+                  initialContentClasses='w-full text-left'
+                >
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">QuizzipIO is perfect for generating quizzes, assessments, exams, and tests. Whether you are a teacher, student, or business, you'll find that the platform is flexible enough for all purposes.</p>
+                    <p className="mb-4">With our AI assessment tool, you can turn any text into a quiz. This means you can use it to create quizzes for studying, training, or evaluating knowledge. Teachers can use it to create quizzes for their students, while businesses can create quizzes for employee assessments.</p>
+                    <p className="mb-4">The platform is also great for generating quick assessments or practice tests. You can use it to generate questions for sample exams, mock quizzes, or interactive study material to help you prepare for exams or improve your understanding of a subject.</p>
+                    <p className='mb-4'>Whether you need to create quizzes for educational purposes, training programs, or marketing campaigns, Quizgecko has got you covered.</p>
+                  </div>
+                </Accordion>
+              </div>
+              <div className="lg:w-1/2">
+                <Accordion 
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">How much text should I add?</div>}
+                  initialContentClasses='w-full text-left'
+                >
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">We recommend that you use a minimum of 500 words. More text will generally result in better questions. If you want to generate up to 12+ questions then you'll generally need over a thousand words. We recommend that you break very long pieces of text into topical categories. You can make a quiz with any number of total questions.</p>
+                    <p className="mb-4">Additionally, if you don't have a specific text to use, you can take advantage of our topic mode. Simply enter a general topic and our AI will search the web and find relevant content for you. This way, you can effortlessly generate quizzes without having to provide your own text.</p>
+                  </div>
+                </Accordion>
+              </div>
+              <div className="lg:w-1/2">
+                <Accordion 
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">How do I share my assessments?</div>}
+                  initialContentClasses='w-full text-left'
+                >
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">We know that often you will need to provide our assessments to other end users. For that, we offer a way to save your tests to a permanent library, that will provide you the option to export the test to a pdf or document file. You can then print the assessment and share that assessment with whomever you wish.</p>
+                  </div>
+                </Accordion>
+              </div>
+              <div className="lg:w-1/2">
+                <Accordion 
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">What text can I use to generate quiz or test questions??</div>}
+                  initialContentClasses='w-full text-left'
+                >
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">QuizzipIO is an AI question generator that can create quizzes from various sources. You can input text from articles, presentations, notes, or even URLs. For Pro users, file uploads are also available. Our system will attempt to extract the text and formulate quiz questions from it. Please ensure that you have the right to use the selected text before proceeding.</p>
+                  </div>
+                </Accordion>
+              </div>
+              <div className="lg:w-1/2">
+                <Accordion 
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">How can I improve the quality of results?</div>}
+                  initialContentClasses='w-full text-left'
+                >
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">Virtually any text will work but using factual text content (as opposed to opinion, discussion or narrative) generally results in better questions. If you notice that the AI is frequently generating incorrect answers, then this is typically because the text is suboptimal.</p>
+                  </div>
+                </Accordion>
+              </div>
+              <div className="lg:w-1/2">
+                <Accordion 
+                  extra_classes="border-2 border-slate-500 mr-4 rounded-lg mb-4 hover:border-slate-300" initialContent={<div className="p-4 hover:underline underline-offset-4 font-bold text-lg">Are the answers always correct?</div>}
+                  initialContentClasses='w-full text-left'
+                >
+                  <div className="text-sm text-slate-300 px-4 pb-4 leading-loose">
+                    <p className="mb-4">If quality, accurate text is used, then it's rare for the AI to make factual errors. It's important to note though, that like humans, the AI can make mistakes and 100% accuracy cannot be guaranteed.</p>
                   </div>
                 </Accordion>
               </div>
